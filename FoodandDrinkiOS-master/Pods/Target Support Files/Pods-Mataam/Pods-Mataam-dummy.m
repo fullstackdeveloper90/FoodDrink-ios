ce5b8445fda89be6e6d1ed559ebbd00cb9bd70d2
fullstackdeveloper90 <clearhand0115@gmail.com>
2018-11-20 16:37:59 +1000

    clear project

--- FoodandDrinkiOS-master/Pods/Target Support Files/Pods-Mataam/Pods-Mataam-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mataam : NSObject
-@end
-@implementation PodsDummy_Pods_Mataam
-@end
--- FoodandDrinkiOS-master/Pods/Target Support Files/Pods-MataamUITests/Pods-MataamUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MataamUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MataamUITests
-@end
